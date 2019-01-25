26a7bff20bf49601c7f2b3b8423a95eca841dd37
Zach Richardson <zach@forsta.io>
2019-01-24 18:27:09 -0700

    updated design with semantic ui

---
+++ src/components/TopMenu.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="ui inverted menu">
+    <router-link class="item" v-for="item in items" :key="item.id" :to="item.route">{{item.name}}</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TopMenu",
+  props: {
+    items: {
+      type: Array,
+      default: function() {
+        return [
+          {
+            id: 1,
+            name: "Home",
+            route: "/"
+          },
+          {
+            id: 2,
+            name: "Login",
+            route: "/login"
+          },
+          {
+            id: 3,
+            name: "Register",
+            route: "/register"
+          }
+        ];
+      }
+    }
+  }
+};
+</script>
