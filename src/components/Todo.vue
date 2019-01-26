301a6cbf613a7f615c37db933e1adbc2440abec4
Zach Richardson <zach@forsta.io>
2019-01-25 21:48:06 -0700

    Finished implementing material design on the client side

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,17 +1,14 @@
 <template>
-    <div class="card">
-      <div class="image">
-        <img src="@/assets/todo.png">
-      </div>
-      <div class="content">
-        <div class="header">{{todo.name}}</div>
-        <div class="description">{{todo.description}}</div>
-      </div>
-      <div class="extra content">
-        <button class="ui button primary" @click="doneTodo(todo)" v-show="!todo.done">Done</button>
-        <button class="ui button negative" @click="removeTodo(todo)">Remove</button>
-      </div>
-    </div>
+  <md-card>
+    <md-card-header>
+      <div class="md-title">{{todo.name}}</div>
+    </md-card-header>
+    <md-card-content>{{todo.description}}</md-card-content>
+    <md-card-actions>
+      <md-button class="md-accent" @click="removeTodo(todo)">Remove</md-button> 
+      <md-button class="md-primary" @click="doneTodo(todo)" v-show="!todo.done">Done</md-button>
+    </md-card-actions>
+  </md-card>
 </template>
 
 <script>
@@ -20,11 +17,15 @@ import { mapActions } from "vuex";
 export default {
   name: "Todo",
   props: { todo: Object },
-  methods: {
-    ...mapActions(["doneTodo", "removeTodo"])
-  }
+  methods: { ...mapActions(["doneTodo", "removeTodo"]) }
 };
 </script>
 
 <style scoped lang="scss">
+.md-card {
+  width: 320px;
+  margin: 4px;
+  display: inline-block;
+  vertical-align: top;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,40 @@
 <template>
-  <div class="ui grid container">
-    <button class="ui button primary" @click="showModal=true">Add Task</button>
-    <div class="ui cards row">
-      <todo v-for="todo in allTodos" :key="todo.id" :todo="todo"></todo>
-    </div>
+  <div>
+    <todo v-for="todo in allTodos" :key="todo.id" :todo="todo"></todo>
 
-    <modal v-model="showModal">
-      <div slot="header">Add Task</div>
-      <div slot="content">
-        <div class="ui form">
-          <div class="field">
-            <label>Name</label>
-            <input type="text" class="form-control" v-model="name">
-          </div>
-          <div class="field">
-            <label>Description</label>
-            <input type="text" class="form-control" v-model="description">
-          </div>
-        </div>
-      </div>
-      <div slot="actions">
-        <div class="ui default deny button" @click="showModal=false">Cancel</div>
-        <div class="ui primary right button" @click="saveTask">Add Task</div>
-      </div>
-    </modal>
+    <md-button class="md-fab md-fab-bottom-right md-primary" @click="showDialog = true">
+      <md-icon>add</md-icon>
+    </md-button>
+
+    <md-dialog :md-active.sync="showDialog">
+      <md-dialog-title>Add Task</md-dialog-title>
+      <md-dialog-content>
+        <md-field>
+          <label>Name</label>
+          <md-input type="text" v-model="name" required/>
+          <span class="md-error" v-show="!name">The task name is required</span>
+        </md-field>
+        <md-field>
+          <label>Description</label>
+          <md-input type="type" v-model="description" required/>
+          <span class="md-error" v-show="!description">The task description is required</span>
+        </md-field>
+      </md-dialog-content>
+      <md-dialog-actions>
+        <md-button class="md-accent" @click="showDialog = false">Cancel</md-button>
+        <md-button class="md-primary" @click="saveTask()">Save</md-button>
+      </md-dialog-actions>
+    </md-dialog>
   </div>
 </template>
 
 <script>
-import Modal from "vue-semantic-modal";
 import Todo from "@/components/Todo.vue";
 import { mapGetters, mapMutations, mapActions } from "vuex";
 
 export default {
   name: "home",
-  components: { Modal, Todo },
+  components: { Todo },
   mounted: mounted,
   data: data,
   methods: {
@@ -48,9 +48,9 @@ export default {
 
 function data() {
   return {
-    showModal: false,
     name: "",
-    description: ""
+    description: "",
+    showDialog: false
   };
 }
 
@@ -67,12 +67,16 @@ function saveTask() {
   });
   this.name = "";
   this.description = "";
-  this.showModal = false;
+  this.showDialog = false;
 }
 </script>
 
 <style lang="scss" scoped>
-.grid {
-  margin-top: 50px;
+button {
+  margin-left: 16px;
+}
+.md-dialog {
+  min-width: 500px;
+  max-width: 768px;
 }
 </style>
