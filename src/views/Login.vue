9f0920c51774602affb7eee032c1ac9937398bad
Zach Richardson <zach@forsta.io>
2019-01-25 20:35:04 -0700

    added material design forms to register and login views

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,44 +1,59 @@
 <template>
-  <div class="ui four column grid container centered">
-    <div class="two column row">
-      <div class="column">
-        <h2>Login</h2>
-      </div>
-    </div>
-    <div class="two column row">
-      <div class="column">
-        <form class="ui form">
-          <div class="field">
-            <label for="email">Email</label>
-            <input type="email" id="email" v-model="email" required>
-          </div>
-          <div class="field">
-            <label for="password">Password</label>
-            <input type="password" id="password" v-model="email" required>
-          </div>
-          <button class="ui button" type="submit">Submit</button>
-        </form>
-      </div>
-    </div>
+  <div>
+    <form class="md-layout md-alignment-top-center" @submit.stop.prevent="login">
+      <md-card class="md-layout-item md-size-50 md-small-size-100">
+        <md-card-header>
+          <div class="md-title">Login</div>
+        </md-card-header>
+
+        <md-card-content>
+          <md-field>
+            <label>Email</label>
+            <md-input type="email" autocomplete="email" v-model="email" required/>
+            <span class="md-error" v-show="!email">The email is required</span>
+          </md-field>
+          <md-field>
+            <label>Password</label>
+            <md-input type="password" autocomplete="password" v-model="password" required/>
+            <span class="md-error" v-show="!password">The password is required</span>
+          </md-field>
+        </md-card-content>
+
+        <md-card-actions>
+          <md-button type="submit" class="md-primary">Log in</md-button>
+        </md-card-actions>
+      </md-card>
+      <md-snackbar md-position="left" md-duration="2000" :md-active.sync="showSnackbar" md-persistent>
+        <span>Welcome Back!</span>
+        <md-button class="md-primary" @click="showSnackbar = false">Thanks</md-button>
+      </md-snackbar>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
   name: "login",
-  data: data
+  data: data,
+  methods: {
+    login
+  }
 };
 
 function data() {
   return {
-    email: "",
-    password: ""
+    email: null,
+    password: null,
+    showSnackbar: false,
+    position: "left",
+    duration: 4000
   };
+}
+
+function login() {
+  this.showSnackbar = true;
 }
 </script>
 
 <style lang="scss" scoped>
-.grid {
-  margin-top: 100px;
-}
 </style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,38 +1,92 @@
 <template>
   <div>
-    <div class="md-layout">
-      <div class="md-layout-item">
-        <h1>Register</h1>
-      </div>
-    </div>
+    <form class="md-layout md-alignment-top-center" @submit.stop.prevent="register">
+      <md-card class="md-layout-item md-size-50 md-small-size-100">
+        <md-card-header>
+          <div class="md-title">Create a Free Account</div>
+        </md-card-header>
 
-    <md-content class="md-scrollbar selection-black">
-      <p>Autem enim asperiores consequuntur neque sequi ea similique ex maxime, repudiandae doloremque aliquam exercitationem omnis assumenda. Rem suscipit pariatur vero facere?</p>
-      <p>Necessitatibus aut cumque sit ad. Tempora perferendis nostrum, in assumenda accusantium vitae vero pariatur sapiente nam quisquam, ducimus distinctio quae nisi.</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed perspiciatis sit quaerat molestiae iusto adipisci possimus cum modi quam qui esse vero provident, ad, deserunt laborum quas eligendi beatae quibusdam.</p>
-    </md-content>
+        <md-card-content>
+          <div class="md-layout md-gutter">
+            <div class="md-layout-item md-small-size-100">
+              <md-field>
+                <label>First Name</label>
+                <md-input type="text" v-model="firstName" required/>
+                <span class="md-error" v-show="!firstName">The first name is required</span>
+              </md-field>
+            </div>
+            <div class="md-layout-item md-small-size-100">
+              <md-field>
+                <label>Last Name</label>
+                <md-input type="text" v-model="lastName" required/>
+                <span class="md-error" v-show="!lastName">The last name is required</span>
+              </md-field>
+            </div>
+          </div>
+          <md-field>
+            <label>Email</label>
+            <md-input type="email" v-model="email" required/>
+            <span class="md-error" v-show="!email">The email is required</span>
+          </md-field>
+          <div class="md-layout md-gutter">
+            <div class="md-layout-item md-small-size-100">
+              <md-field>
+                <label>Password</label>
+                <md-input type="password" v-model="password" required/>
+                <span class="md-error" v-show="!password">The password is required</span>
+              </md-field>
+            </div>
+            <div class="md-layout-item md-small-size-100">
+              <md-field>
+                <label>Confirm Password</label>
+                <md-input type="password" v-model="confirmPassword" required/>
+                <span
+                  class="md-error"
+                  v-show="confirmPassword !== password"
+                >The passwords don't match</span>
+              </md-field>
+            </div>
+          </div>
+        </md-card-content>
+
+        <md-card-actions>
+          <md-button type="submit" class="md-primary">Create Account</md-button>
+        </md-card-actions>
+      </md-card>
+      <md-snackbar md-position="left" md-duration="2000" :md-active.sync="showSnackbar" md-persistent>
+        <span>Your account was created successfully!</span>
+        <md-button class="md-primary" @click="showSnackbar = false">Okay</md-button>
+      </md-snackbar>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
   name: "register",
-  data: data
+  data: data,
+  methods: {
+    register
+  }
 };
 
 function data() {
   return {
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    confirmPassword: ""
+    firstName: null,
+    lastName: null,
+    email: null,
+    password: null,
+    confirmPassword: null,
+    showSnackbar: false,
+    position: "left",
+    duration: 4000
   };
+}
+
+function register() {
+  this.showSnackbar = true;
 }
 </script>
 
 <style lang="scss" scoped>
-  .md-content {
-    padding: 1px 16px;
-  }
 </style>
