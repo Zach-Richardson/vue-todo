ff01392c20ed12368a47a3d1310529af5bb7ad1b
Zach Richardson <zach@forsta.io>
2019-01-25 23:32:55 -0700

    updated api file

--- server/api.js
+++ server/api.js
@@ -1,15 +1,1 @@
 const { Account, Task } = require("./db.js");
-
-Account.create({
-  id: "0c14e7b4-212d-11e9-ab14-d663bd873d93",
-  firstName: "Zach",
-  lastName: "Richardson",
-  email: "zach@forsta.io",
-  password: "password"
-});
-
-Task.create({
-  name: "Something to Do",
-  description: "This is something I really need to do",
-  done: false
-});
