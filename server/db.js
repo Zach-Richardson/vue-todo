5b252c3bfc1eec3fac7c30d2d35b61ccebe0b9fa
Zach Richardson <zach@forsta.io>
2019-01-27 21:48:50 -0700

    moved database stuff into database folder

--- server/db.js
+++ server/db.js
@@ -12,20 +12,47 @@ const sequelize = new Sequelize("postgres", "postgres", "postgres", {
   }
 });
 
-const Account = sequelize.define("Account", {
-  id: { type: Sequelize.UUID, primaryKey: true },
-  firstName: { type: Sequelize.STRING },
-  lastName: { type: Sequelize.STRING },
-  email: { type: Sequelize.STRING },
-  password: { type: Sequelize.STRING }
-});
+const Account = sequelize.define(
+  "Account",
+  {
+    id: { type: Sequelize.UUID, primaryKey: true },
+    firstName: { type: Sequelize.STRING },
+    lastName: { type: Sequelize.STRING },
+    email: { type: Sequelize.STRING },
+    password: { type: Sequelize.STRING },
+    tasks: { type: Sequelize.ARRAY(Sequelize.STRING) }
+  },
+  {
+    indexes: [
+      {
+        unique: true,
+        fields: ["id"]
+      },
+      {
+        unique: true,
+        fields: ["email"]
+      }
+    ]
+  }
+);
 
-const Task = sequelize.define("Task", {
-  id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-  name: { type: Sequelize.STRING },
-  description: { type: Sequelize.STRING },
-  done: { type: Sequelize.STRING }
-});
+const Task = sequelize.define(
+  "Task",
+  {
+    id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: Sequelize.STRING },
+    description: { type: Sequelize.STRING },
+    done: { type: Sequelize.STRING }
+  },
+  {
+    indexes: [
+      {
+        unique: true,
+        fields: ["id"]
+      }
+    ]
+  }
+);
 
 Account.sync();
 Task.sync();
